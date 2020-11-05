441091cd05198549a22d352fb74635bd1e75ace0
Csontos Róbert <nagyrobert1996@gmail.com>
2020-11-05 20:25:06 +0100

    Új dolgozó controller alapjának elkészítése

--- src/main/java/controllers/NewWorkerController.java
+++ src/main/java/controllers/NewWorkerController.java
@@ -1,4 +1,27 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class NewWorkerController {
+
+    @FXML
+    private TextField workerName;
+
+    @FXML
+    private TextField workerEmail;
+
+    @FXML
+    private TextField workerPhoneNumber;
+
+    @FXML
+    private DatePicker workerBirthDate;
+
+    @FXML
+    void newWorkerSave(MouseEvent event) {
+
+    }
+
 }
