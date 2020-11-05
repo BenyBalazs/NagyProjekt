37e95431cdcb6386155db09436c0030d9a44f671
Csontos Róbert <nagyrobert1996@gmail.com>
2020-11-05 21:39:02 +0100

    Dolgozók és autók és tulajok layoutok hozzákapcsolása a menü layouthoz

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -2,9 +2,21 @@ package controllers;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class MenuController {
+
+    private static Logger logger = LoggerFactory.getLogger("MenuController.class");
+
+    @FXML
+    private BorderPane borderPane;
 
     @FXML
     void newRecord(MouseEvent event) {
@@ -18,12 +30,22 @@ public class MenuController {
 
     @FXML
     void ownerAndCar(MouseEvent event) {
-
+        loadUi("ownerAndCar");
     }
 
     @FXML
     void worker(MouseEvent event) {
+        loadUi("worker");
+    }
 
+    private void loadUi(String ui){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(ui + ".fxml"));
+        } catch (IOException ex) {
+            logger.error("A hiba forrása {}", ex.toString());
+        }
+        borderPane.setCenter(root);
     }
 
 }
