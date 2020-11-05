e30b7bd0e96bdb8ce378d7452416259e96edb551
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-05 19:43:25 +0100

    Repair.java fájl létrehozása.

---
+++ src/main/java/models/Repair.java
@@ -0,0 +1,14 @@
+package models;
+
+import lombok.*;
+import org.hibernate.annotations.Check;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Repair{
+}
