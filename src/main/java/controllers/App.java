1332de5358368cd1546b3244c1c324eaeca00ac1
Csontos Róbert <nagyrobert1996@gmail.com>
2020-11-05 21:41:07 +0100

    App.java osztály elkészítése

--- src/main/java/controllers/App.java
+++ src/main/java/controllers/App.java
@@ -13,22 +13,14 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Műhelyi adminisztráció");
+        stage.setResizable(false);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
 }
