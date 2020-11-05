617ec175acb797e93605ff561f6208bae5c7af0c
Csontos Róbert <nagyrobert1996@gmail.com>
2020-11-05 20:38:12 +0100

    Dolgozók controller alapjának megadása

--- src/main/java/controllers/WorkerController.java
+++ src/main/java/controllers/WorkerController.java
@@ -1,4 +1,22 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
 public class WorkerController {
+
+    @FXML
+    private ListView<?> listOfWorkers;
+
+    @FXML
+    void deleteWorker(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editWorker(MouseEvent event) {
+
+    }
+
 }
