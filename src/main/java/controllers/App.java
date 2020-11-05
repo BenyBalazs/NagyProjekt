585fed1ea860f8f456cdca9671533c933e2c463a
Csontos Róbert <nagyrobert1996@gmail.com>
2020-11-05 22:17:09 +0100

    Minimum ablak méret beállítása az App.java osztályban

--- src/main/java/controllers/App.java
+++ src/main/java/controllers/App.java
@@ -19,6 +19,8 @@ public class App extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Műhelyi adminisztráció");
+        stage.setMinWidth(700);
+        stage.setMinHeight(500);
         stage.show();
     }
 
