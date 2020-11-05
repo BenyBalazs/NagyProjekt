914659c870f180810e78a3ac527f9628e3f52c57
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-05 19:44:00 +0100

    Repair.java osztályannotációk hozzáadása.

--- src/main/java/models/Repair.java
+++ src/main/java/models/Repair.java
@@ -10,5 +10,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Repair{
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Repair {
+
 }
