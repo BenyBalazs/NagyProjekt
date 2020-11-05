1f18a1faaced10887c64c74effc6c48b8e0b264b
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-05 19:02:14 +0100

    CarOwner.java osztály létrehozva.

---
+++ src/main/java/models/CarOwner.java
@@ -0,0 +1,12 @@
+package models;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+public class CarOwner{
+}
