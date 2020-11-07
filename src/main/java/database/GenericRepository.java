6b4e14d59531fa4ac24beb6dc5a1d722accc02b2
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-07 18:52:33 +0100

    Új entitás feltöltése megírva.

--- src/main/java/database/GenericRepository.java
+++ src/main/java/database/GenericRepository.java
@@ -1,8 +1,26 @@
 package database;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Transactional
-public interface GenericRepository {
+public abstract class GenericRepository<T> {
 
+    private static final Logger logger = LoggerFactory.getLogger("MenuController.class");
+
+    public void createNew(T entity){
+        EntityManager em = EmfHelper.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.persist(entity);
+            logger.trace("Új entitás feltöltése sikeres");
+        }catch (Exception e){
+            logger.error("Hiba történt új entitás feltöltése közben {}", e.toString());
+        }finally {
+            em.close();
+        }
+    }
 }
