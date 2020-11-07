e4e09d5e38660ec9042537a586548aa6e2d45549
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-07 18:58:41 +0100

    alap törlő metódus implementálva.

--- src/main/java/database/GenericRepository.java
+++ src/main/java/database/GenericRepository.java
@@ -16,9 +16,24 @@ public abstract class GenericRepository<T> {
         try{
             em.getTransaction().begin();
             em.persist(entity);
+            em.getTransaction().commit();
             logger.trace("Új entitás feltöltése sikeres");
         }catch (Exception e){
             logger.error("Hiba történt új entitás feltöltése közben {}", e.toString());
+        }finally {
+            em.close();
+        }
+    }
+
+    public void simpleDelete(T entity){
+        EntityManager em = EmfHelper.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.remove(em.contains(entity) ? entity : em.merge(entity));
+            em.getTransaction().commit();
+            logger.trace("A szimpla törlés sikeresen megtörtént.");
+        }catch (Exception e){
+            logger.error("Hiba történt az entitás törlése közben: {}", e.toString());
         }finally {
             em.close();
         }
