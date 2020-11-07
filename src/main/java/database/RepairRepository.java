803ff08a820742579c9b9539a732f0d299650fc2
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-07 21:36:40 +0100

    RepairRepository láthetóságának módosítása.

--- src/main/java/database/RepairRepository.java
+++ src/main/java/database/RepairRepository.java
@@ -5,5 +5,5 @@ import models.Repair;
 import javax.transaction.Transactional;
 
 @Transactional
-public class RepairRepository extends GenericRepository<Repair> {
+class RepairRepository extends GenericRepository<Repair> {
 }
