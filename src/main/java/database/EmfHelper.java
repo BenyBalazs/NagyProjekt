b4ff3c5ab35fabc2ba867f167807457559f3c761
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-07 18:15:59 +0100

    EmfHelper függvényeinek és válltozóinak megírása.

--- src/main/java/database/EmfHelper.java
+++ src/main/java/database/EmfHelper.java
@@ -1,4 +1,20 @@
 package database;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class EmfHelper {
+
+    private EmfHelper(){};
+
+    private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Cars");
+
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+
+    public static void closeEmf(){
+        emf.close();
+    }
 }
