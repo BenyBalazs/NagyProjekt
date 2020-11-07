6165a8db4b10dacd744b1dd676e0d8b122bb001c
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-07 21:40:58 +0100

    Repositories osztály változóinak felvétele.

--- src/main/java/database/Repositories.java
+++ src/main/java/database/Repositories.java
@@ -3,4 +3,10 @@ package database;
 public final class Repositories {
 
     private Repositories(){}
+
+    public static CarRepository carRepository = new CarRepository();
+    public static CarModelRepository carModelRepository = new CarModelRepository();
+    public static CarOwnerRepository carOwnerRepository = new CarOwnerRepository();
+    public static MechanicRepository mechanicRepository = new MechanicRepository();
+    public static RepairRepository repairRepository = new RepairRepository();
 }
