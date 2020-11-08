68c523a013eb7b7d44b37bf2def1d3504ea619e1
Patrik Kis <kisp13@gmail.com>
2020-11-08 20:26:16 +0100

    NewWorker controller elkészült.

--- src/main/java/controllers/NewWorkerController.java
+++ src/main/java/controllers/NewWorkerController.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import database.Repositories;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import models.Mechanic;
 
 public class NewWorkerController {
 
@@ -21,7 +23,12 @@ public class NewWorkerController {
 
     @FXML
     void newWorkerSave(MouseEvent event) {
-
+        Mechanic mechanic = new Mechanic();
+        mechanic.setName(workerName.getText());
+        mechanic.setEmail(workerEmail.getText());
+        mechanic.setPhoneNum(workerPhoneNumber.getText());
+        mechanic.setDateOfBirth(workerBirthDate.getValue());
+        Repositories.mechanicRepository.createNew(mechanic);
     }
 
 }
