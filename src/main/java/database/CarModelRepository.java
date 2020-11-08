9daf9038a1e4399f315924b6f48969a5be14e6d9
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-08 11:41:10 +0100

    Auó tulajdonosok erőszakos törlése implementálva.

--- src/main/java/database/CarModelRepository.java
+++ src/main/java/database/CarModelRepository.java
@@ -4,15 +4,13 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import models.Car;
 import models.CarModel;
-
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Transactional
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class CarModelRepository extends GenericRepository<CarModel> {
 
-    public void forceDelete(CarModel entity){
+    public void deleteAndSetConnectionToNull(CarModel entity){
         for(Car a : entity.getCars()){
            try {
                a.setModel(null);
--- src/main/java/database/CarOwnerRepository.java
+++ src/main/java/database/CarOwnerRepository.java
@@ -2,6 +2,8 @@ package database;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import models.Car;
+import models.CarModel;
 import models.CarOwner;
 
 import javax.transaction.Transactional;
@@ -9,4 +11,18 @@ import javax.transaction.Transactional;
 @Transactional
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class CarOwnerRepository extends GenericRepository<CarOwner> {
+
+    public void deleteAndSetConnectionToNull(CarOwner entity){
+        for(Car a : entity.getOwnedCars()){
+            if(a == null)
+                break;
+
+            try {
+                a.setModel(null);
+                Repositories.carRepository.commitChange(a);
+            }catch (Exception e) {System.out.println("Üres Lisa");}
+
+        }
+        Repositories.carOwnerRepository.simpleDelete(entity);
+    }
 }
