c18989e36caf58512a87460da45fd3212c31bd4b
Benyovszki Balazs <gnuki2000@gmail.com>
2020-11-08 18:23:18 +0100

    Autómodell kereső függvény megírása.

--- src/main/java/database/CarModelRepository.java
+++ src/main/java/database/CarModelRepository.java
@@ -4,11 +4,22 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import models.Car;
 import models.CarModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 @Transactional
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class CarModelRepository extends GenericRepository<CarModel> {
+
+    private static final Logger logger = LoggerFactory.getLogger("GenericRepository<T>.class");
 
     public void deleteAndSetConnectionToNull(CarModel entity) {
         if (!entity.getCars().isEmpty()) {
@@ -18,5 +29,26 @@ public class CarModelRepository extends GenericRepository<CarModel> {
             }
         }
         Repositories.carModelRepository.simpleDelete(entity);
+    }
+
+    public CarModel getByBrandAndType(String brand, String type){
+        EntityManager em = EmfHelper.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
+        CriteriaQuery<CarModel> cq = cb.createQuery(CarModel.class);
+
+        Root<CarModel> from = cq.from(CarModel.class);
+
+        cq.select(from).where(cb.equal(from.get("brand"), brand),cb.equal(from.get("type"),type));
+
+        try{
+            Query q = em.createQuery(cq);
+            return (CarModel) q.getResultList().get(0);
+        }catch (Exception e){
+            logger.error("Hiba a modell megtalálása közben.");
+        }finally {
+            em.close();
+        }
+        return null;
     }
 }
