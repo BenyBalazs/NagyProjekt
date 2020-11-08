3127f8ad82090fc7379b1cd4dec38856f829d051
Patrik Kis <kisp13@gmail.com>
2020-11-08 20:45:36 +0100

    CarsAndOwnersController megírása

--- src/main/java/controllers/CarsAndOwnersController.java
+++ src/main/java/controllers/CarsAndOwnersController.java
@@ -1,10 +1,16 @@
 package controllers;
 
 import database.Repositories;
+import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import models.Car;
+import models.CarOwner;
 
 public class CarsAndOwnersController {
 
@@ -12,10 +18,27 @@ public class CarsAndOwnersController {
     private TextField searchField;
 
     @FXML
-    private ListView<?> listOfCars;
+    private ListView<Car> listOfCars;
 
     @FXML
-    private ListView<?> listOfOwners;
+    private ListView<CarOwner> listOfOwners;
+
+    @FXML
+    public void initialize(){
+        listOfOwners.setItems(FXCollections.observableList(Repositories.carOwnerRepository.findAll()));
+        listOfOwners.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        listOfOwners.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+                    if(mouseEvent.getClickCount() == 2){
+                        CarOwner owner = listOfOwners.getSelectionModel().getSelectedItem();
+                        listOfCars.setItems(FXCollections.checkedObservableList(owner.getOwnedCars()));
+                    }
+                }
+            }
+        });
+    }
 
     @FXML
     void searchOwner(MouseEvent event) {
--- src/main/java/controllers/WorkerController.java
+++ src/main/java/controllers/WorkerController.java
@@ -1,13 +1,21 @@
 package controllers;
 
+import database.Repositories;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import models.Mechanic;
 
 public class WorkerController {
 
     @FXML
-    private ListView<?> listOfWorkers;
+    private ListView<Mechanic> listOfWorkers;
+
+    @FXML
+    public void initialize(){
+        listOfWorkers.setItems(FXCollections.observableList(Repositories.mechanicRepository.findAll()));
+    }
 
     @FXML
     void deleteWorker(MouseEvent event) {
