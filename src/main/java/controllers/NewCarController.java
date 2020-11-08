f13d57337439318f377e3779d5e8e4241120c0b1
Patrik Kis <kisp13@gmail.com>
2020-11-08 18:57:43 +0100

    New car controller megírásának elkezdése.

--- src/main/java/controllers/NewCarController.java
+++ src/main/java/controllers/NewCarController.java
@@ -1,19 +1,22 @@
 package controllers;
 
+import database.Repositories;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import models.Car;
+import models.CarModel;
 
 public class NewCarController {
 
     @FXML
-    private ComboBox<?> model;
+    private ComboBox<String> brand;
 
     @FXML
-    private ComboBox<?> type;
+    private ComboBox<String> type;
 
     @FXML
     private TextField licencePlate;
@@ -26,6 +29,17 @@ public class NewCarController {
 
     @FXML
     void ownerAndCarSave(MouseEvent event) {
+        Car car = new Car();
+        car.setLicensePlate(licencePlate.getText());
+        car.setManufacturedDate(manufacturedDate.getValue());
+        CarModel model = Repositories.carModelRepository.getByBrandAndType(brand.getValue(),type.getValue());
+        if(model == null) {
+            model = new CarModel();
+            model.setBrand(brand.getValue());
+            model.setType(type.getValue());
+            Repositories.carModelRepository.createNew(model);
+        }
+        car.setModel(model);
 
     }
 
