404924198d3308569b62a414cdde0f7a3a8c21f2
Csontos Róbert <nagyrobert1996@gmail.com>
2020-11-26 11:02:33 +0100

    WorkflowController.java alapjának elkészítése, és a layouttal való összekapcsolás.

--- src/main/java/controllers/WorkflowController.java
+++ src/main/java/controllers/WorkflowController.java
@@ -1,4 +1,22 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
 public class WorkflowController {
+
+    @FXML
+    private ListView<?> listOfRepairs;
+
+    @FXML
+    void deleteRepair(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editRepair(MouseEvent event) {
+
+    }
+
 }
