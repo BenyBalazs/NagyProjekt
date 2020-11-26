264b6defb3f3e15c8c63efcaf12d3aadd78fc238
Csontos Róbert <nagyrobert1996@gmail.com>
2020-11-26 10:40:13 +0100

    EditOwnerController.java alapjának megírása

--- src/main/java/controllers/EditOwnerController.java
+++ src/main/java/controllers/EditOwnerController.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class EditOwnerController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField address;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private DatePicker birthDate;
+
+    @FXML
+    void saveEditOwner(MouseEvent event) {
+
+    }
+
 }
