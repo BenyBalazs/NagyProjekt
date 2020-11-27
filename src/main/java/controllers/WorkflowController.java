5d6ce7b326ef417d31f7f21a3615038bac47c0b1
Benyovszki_Balazs <gnuki2000@gmail.com>
2020-11-27 13:23:11 +0100

    WorkflowController ? típus cserélése a megfelelő típusra.

--- src/main/java/controllers/WorkflowController.java
+++ src/main/java/controllers/WorkflowController.java
@@ -3,11 +3,12 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import models.Repair;
 
 public class WorkflowController {
 
     @FXML
-    private ListView<?> listOfRepairs;
+    private ListView<Repair> listOfRepairs;
 
     @FXML
     void deleteRepair(MouseEvent event) {
