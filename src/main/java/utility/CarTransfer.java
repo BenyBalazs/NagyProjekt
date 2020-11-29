2e3faf851d1bf48bd6bd1c54e91a403e7d6ae477
Patrik Kis <kisp13@gmail.com>
2020-11-29 16:39:36 +0100

    CarTransfer osztály megírása

--- src/main/java/utility/CarTransfer.java
+++ src/main/java/utility/CarTransfer.java
@@ -1,4 +1,11 @@
 package utility;
 
-public class CarTransfer {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import models.Car;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CarTransfer {
+
+    public static Car carTransfer = null;
 }
