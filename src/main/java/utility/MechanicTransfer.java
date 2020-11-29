01fd9dc21d9b824c5f55050cbeca32f7d4342f77
Patrik Kis <kisp13@gmail.com>
2020-11-29 16:10:27 +0100

    MechanicTransfer.java file megírása

--- src/main/java/utility/MechanicTransfer.java
+++ src/main/java/utility/MechanicTransfer.java
@@ -1,1 +1,11 @@
+package utility;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import models.Mechanic;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MechanicTransfer {
+
+    public static Mechanic mechanicTransfer = null;
+}
