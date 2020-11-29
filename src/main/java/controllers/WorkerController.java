568276433758ddaa333b84f4cf1178f1db4c9f05
Patrik Kis <kisp13@gmail.com>
2020-11-29 13:50:10 +0100

    deleteWorker megírása a WorkerController.java filehoz

--- src/main/java/controllers/WorkerController.java
+++ src/main/java/controllers/WorkerController.java
@@ -3,11 +3,19 @@ package controllers;
 import database.Repositories;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import models.Mechanic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class WorkerController {
+
+    private static Logger logger = LoggerFactory.getLogger("WorkerController.class");
 
     @FXML
     private ListView<Mechanic> listOfWorkers;
@@ -19,7 +27,20 @@ public class WorkerController {
 
     @FXML
     void deleteWorker(MouseEvent event) {
+        Alert confirmDelete = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmDelete.setTitle("Törlés megerősítése!");
+        confirmDelete.setContentText("A törlés visszaállíthatatlanul eltávolítja az objektumot az adatbázisból és anomáliákhoz vezethet! Kérjük győződjön meg arról, hogy a szerelőhöz nem tartozik szerelés!");
+        ButtonType confirm = new ButtonType("Törölje az adatbázisból!");
+        ButtonType cancel = new ButtonType("Mégse");
+        confirmDelete.getButtonTypes().setAll(confirm,cancel);
 
+        Optional<ButtonType> result = confirmDelete.showAndWait();
+
+        if(result.get() == confirm){
+            logger.trace("A felhasználó törölni kívánta az entitást!");
+            Repositories.mechanicRepository.deleteAndSetConnectionToNull(listOfWorkers.getSelectionModel().getSelectedItem());
+
+        }
     }
 
     @FXML
