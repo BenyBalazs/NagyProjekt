6bd9b9b79b533478ff1016d625dfcd7ba240c509
Patrik Kis <kisp13@gmail.com>
2020-11-29 13:26:51 +0100

    Initialize függvény megírása az EditOwnerController.java filehoz.

--- src/main/java/controllers/EditOwnerController.java
+++ src/main/java/controllers/EditOwnerController.java
@@ -1,11 +1,21 @@
 package controllers;
 
+import database.Repositories;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utility.OwnerTransfer;
+
+import java.util.Optional;
 
 public class EditOwnerController {
+
+    private static final Logger logger = LoggerFactory.getLogger("EditOwnerController.class");
 
     @FXML
     private TextField name;
@@ -21,6 +31,15 @@ public class EditOwnerController {
 
     @FXML
     private DatePicker birthDate;
+
+    @FXML
+    public void initialize(){
+        name.setText(OwnerTransfer.ownerTransfer.getName());
+        address.setText(OwnerTransfer.ownerTransfer.getAddress());
+        phoneNumber.setText(OwnerTransfer.ownerTransfer.getPhoneNum());
+        email.setText(OwnerTransfer.ownerTransfer.getEmail());
+        birthDate.setValue(OwnerTransfer.ownerTransfer.getDateOfBirth());
+    }
 
     @FXML
     void deleteOwner(MouseEvent event) {
