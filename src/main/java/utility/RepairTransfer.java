ea45a4628438dce5a853d600e04e8ba4d21053be
Patrik Kis <kisp13@gmail.com>
2020-11-29 16:56:16 +0100

    RepairTransfer osztály megírása

--- src/main/java/utility/RepairTransfer.java
+++ src/main/java/utility/RepairTransfer.java
@@ -1,5 +1,10 @@
 package utility;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import models.Car;
+import models.Repair;
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 
 public final class RepairTransfer {
 
