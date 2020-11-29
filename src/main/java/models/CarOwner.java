3322cdb52b98fe034aacb315052b6787b5bb787a
Patrik Kis <kisp13@gmail.com>
2020-11-29 12:40:59 +0100

    CarOwner.java to string metódus kiegészítése

--- src/main/java/models/CarOwner.java
+++ src/main/java/models/CarOwner.java
@@ -16,18 +16,17 @@ public class CarOwner {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.PRIVATE)
     Integer carOwnerId;
     String name;
     String address;
     LocalDate dateOfBirth;
     String email;
     String phoneNum;
-    @OneToMany(mappedBy = "owner")
+    @OneToMany(mappedBy = "owner",fetch = FetchType.EAGER)
     List<Car> ownedCars = new ArrayList<>();
 
     @Override
     public String toString() {
-        return name + " " + (dateOfBirth == null ? " " : dateOfBirth.toString());
+        return carOwnerId + " " + name + " " + (dateOfBirth == null ? " " : dateOfBirth.toString());
     }
 }
